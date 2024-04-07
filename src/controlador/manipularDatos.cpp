14599550f37de1457c7fbb49c40c35f4ab68d011
Marcelo Perozo <marceloperozocx@gmail.com>
2024-04-07 12:05:34 -0400

    fix: Se corrigio la funcion manipularDatos.cpp y operaciones.cpp

--- src/controlador/manipularDatos.cpp
+++ src/controlador/manipularDatos.cpp
@@ -4,28 +4,24 @@
 #include "../modelo/estilos.h"
 using namespace std;
 
-void buscarCliente(int& id_cliente, int numero_clientes, Usuarios clientes[], bool estado){
-    styleOutput("italic", "yellow","Ingresa el 'ID' del cliente para buscar su informacion:");
-    id_cliente = styleIputInt("yellow");
-
+void buscarCliente(int& id_cliente, int numero_clientes, Usuarios clientes[], bool estado)
+{
     int low = 0, high = numero_clientes - 1;
     id_cliente = binarySearch(low, high, clientes, id_cliente, estado);
 
     if(id_cliente == -1){
-        styleOutput("","rojo","""[ERROR]: No existe un cliente con el ID ingresado.");
+        styleOutput("bold","rojo","""[ERROR]: No existe un cliente con el ID ingresado.");cout<<endl<<endl;
         exit(0);
     }
 }
 
-void buscarCarro(int& id_carro, int numero_carros, Autos carros[], bool estado){
-    styleOutput("italic", "yellow","Ingresa el 'ID' del carro para buscar su informacion:");
-    id_carro = styleIputInt("yellow");
-
+void buscarCarro(int& id_carro, int numero_carros, Autos carros[], bool estado)
+{
     int low = 0, high = numero_carros - 1;
     id_carro = binarySearch(low, high, carros, id_carro, estado);
 
     if(id_carro == -1){
-        styleOutput("","rojo","""[ERROR]: No existe un carro con el ID ingresado.");
+        styleOutput("bold","rojo","""[ERROR]: No existe un carro con el ID ingresado.");cout<<endl<<endl;
         exit(0);
     }
 }
--- src/controlador/operaciones.cpp
+++ src/controlador/operaciones.cpp
@@ -14,76 +14,90 @@ using namespace std;
 
 //CASO 1 - CARROS COMPRADOS Y VENDIDOS POR UN CLIENTE
 
-void mostrarHistorialCliente(int numero_clientes, int numero_carros, Usuarios clientes[], Autos carros[], bool& reiniciar){
+void mostrarHistorialCliente(int numero_clientes, int numero_carros, Usuarios clientes[], Autos carros[], bool& reiniciar)
+{
+    //VARIABLES A UTILIZAR
 
-                //VARIABLES A UTILIZAR
+    int id_cliente, carros_vendidos = 0, carros_comprados = 0; bool estado;
 
-                int id_cliente, carros_vendidos = 0, carros_comprados = 0; bool estado = false;
-
-                //BUSCAMOS EL CLIENTE
+    //BUSCAMOS EL CLIENTE
                 
-                buscarCliente(id_cliente, numero_clientes, clientes, estado);
+    styleOutput("italic", "yellow","Ingresa el 'ID' del cliente para buscar su informacion:");
+    id_cliente = styleIputInt("yellow");
+    buscarCliente(id_cliente, numero_clientes, clientes, estado);
                 
-                // BUSCAMOS LOS CARROS COMPRADOS Y VENDIDOS DEL CLIENTE
+    // BUSCAMOS LOS CARROS COMPRADOS Y VENDIDOS DEL CLIENTE
 
-                for (int i = 0; i < numero_carros; i++) {
-                    if (clientes[id_cliente].id == carros[i].id_vendido) {
-                        carros_vendidos++;
-                    }
+    for (int i = 0; i < numero_carros; i++)
+    {
+        if (clientes[id_cliente].id == carros[i].id_vendido)
+        {
+            carros_vendidos++;
+        }
                         
-                    if (clientes[id_cliente].id == carros[i].id_comprado) {
-                        carros_comprados++;
-                    }
-                }
+        if (clientes[id_cliente].id == carros[i].id_comprado) 
+        {
+            carros_comprados++;
+        }
+    }
 
-                //MOSTRAMOS LOS CARROS COMPRADOS Y VENIDOS POR EL CLIENTE
-                styleOutput("bold","","Cliente:");cout<<endl<<endl;clientes[id_cliente].mostrarDatos(clientes[id_cliente].id, clientes[id_cliente].nombre, clientes[id_cliente].apellido, clientes[id_cliente].email, clientes[id_cliente].edad);cout<<endl<<endl;
-                styleOutput("bold","","Carros vendidos:");cout<<carros_vendidos<<endl<<endl;
-                for(int i = 0; i < numero_carros; i++){
-                    if(clientes[id_cliente].id == carros[i].id_vendido)
-                    {
-                        carros[i].mostrarDatos(carros[i].id, carros[i].marca, carros[i].modelo, carros[i].year, carros[i].id_comprado, carros[i].id_vendido, carros[i].precio_comprado, carros[i].precio_vendido);cout<<endl;
-                    }
-                }
+    //MOSTRAMOS LOS CARROS COMPRADOS Y VENIDOS POR EL CLIENTE
 
-                styleOutput("bold","","Carros comprados:");cout<<carros_comprados<<endl<<endl;
-                for(int i = 0; i < numero_carros; i++){
-                    if(clientes[id_cliente].id == carros[i].id_comprado){
-                        carros[i].mostrarDatos(carros[i].id, carros[i].marca, carros[i].modelo, carros[i].year, carros[i].id_comprado, carros[i].id_vendido, carros[i].precio_comprado, carros[i].precio_vendido);cout<<endl;
-                    }
-                }
+    styleOutput("bold","","Cliente:");cout<<endl<<endl;clientes[id_cliente].mostrarDatos(clientes[id_cliente].id, clientes[id_cliente].nombre, clientes[id_cliente].apellido, clientes[id_cliente].email, clientes[id_cliente].edad);cout<<endl<<endl;
+    styleOutput("bold","","Carros vendidos:");cout<<carros_vendidos<<endl<<endl;
+    for(int i = 0; i < numero_carros; i++)
+    {
+        if(clientes[id_cliente].id == carros[i].id_vendido)
+            {
+                carros[i].mostrarDatos(carros[i].id, carros[i].marca, carros[i].modelo, carros[i].year, carros[i].id_comprado, carros[i].id_vendido, carros[i].precio_comprado, carros[i].precio_vendido);cout<<endl;
+            }
+        }
 
-            reiniciar = false;    
+    styleOutput("bold","","Carros comprados:");cout<<carros_comprados<<endl<<endl;
+    for(int i = 0; i < numero_carros; i++)
+    {
+        if(clientes[id_cliente].id == carros[i].id_comprado){
+            carros[i].mostrarDatos(carros[i].id, carros[i].marca, carros[i].modelo, carros[i].year, carros[i].id_comprado, carros[i].id_vendido, carros[i].precio_comprado, carros[i].precio_vendido);cout<<endl;
+        }
+    }
+
+    reiniciar = false;    
 }
 
 //CASO 2 - MOSTRAR DATOS DEL VENDEDOR Y COMPRADOR DE UN CARRO
 
-void mostrarHistorialCarro(int numero_clientes, int numero_carros, Usuarios clientes[], Autos carros[], bool& reiniciar){
-    
+void mostrarHistorialCarro(int numero_clientes, int numero_carros, Usuarios clientes[], Autos carros[], bool& reiniciar)
+{
     //VARIABLES A UTILIZAR
 
     int id_carro; bool estado = false;
 
     //BUSCAMOS EL ID DEL CARRO
 
+    styleOutput("italic", "yellow","Ingresa el 'ID' del carro para buscar su informacion:");
+    id_carro = styleIputInt("yellow");
     buscarCarro(id_carro, numero_carros, carros, estado);
 
     //BUSCAMOS EL CARRO POR SU ID Y LUEGO COMPARAMOS A SU COMPRADOR Y VENDEDOR
 
-    for(int i = 0; i < numero_clientes; i++){
-        if(carros[id_carro].id_comprado == clientes[i].id){
-            cout<<"Datos del carro:"<<endl<<endl;   
+    for(int i = 0; i < numero_clientes; i++)
+    {
+        if(carros[id_carro].id_comprado == clientes[i].id)
+        {
+            styleOutput("Bold","","Datos del carro:");cout<<endl<<endl;
             carros[i].mostrarDatos(carros[i].id, carros[i].marca, carros[i].modelo, carros[i].year, carros[i].id_comprado, carros[i].id_vendido, carros[i].precio_comprado, carros[i].precio_vendido);cout<<endl;
-            cout<<"Datos del comprador:"<<endl<<endl;
+            styleOutput("Bold","","Datos del comprador:");cout<<endl<<endl;
             clientes[i].mostrarDatos(clientes[i].id, clientes[i].nombre, clientes[i].apellido, clientes[i].email, clientes[i].edad);cout<<endl<<endl;   
-            }         
+        }         
     }
 
-    for(int i = 0; i < numero_clientes; i++){
-            if(carros[id_carro].id_vendido == clientes[i].id){
-                cout<<"Datos del vendedor:"<<endl<<endl;
-                clientes[i].mostrarDatos(clientes[i].id, clientes[i].nombre, clientes[i].apellido, clientes[i].email, clientes[i].edad);cout<<endl<<endl;   
-            }       
+    for(int i = 0; i < numero_clientes; i++)
+    {
+        if(carros[id_carro].id_vendido == clientes[i].id)
+        {
+            styleOutput("Bold","","Datos del vendedor:");cout<<endl<<endl;
+            clientes[i].mostrarDatos(clientes[i].id, clientes[i].nombre, clientes[i].apellido, clientes[i].email, clientes[i].edad);cout<<endl<<endl;   
+        }       
     }
     reiniciar = false;
 }
